83d1ad802e7bab586c0f3bcc91110037a3f1b0ec
Zachary Zee <Oneman2feet@gmail.com>
2013-09-14 02:09:39 -0400

    moving background

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+var canvas = document.getElementsByTagName('canvas')[0];
+var context = canvas.getContext('2d');
+
+//enviornment vars
+var mousePos = {x:150,y:300};
+
 function draw()
 {
     $("canvas").drawImage({
         source: "resources/testbackground.jpg",
-        x: 150, y: 300
+        x: mousePos.x, y: 300
     });
 }
 
-draw();
+function getMousePos(canvas, evt)
+{
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
+
+canvas.addEventListener('mousemove', function(evt)
+{
+    mousePos = getMousePos(canvas, evt);
+}, false);
+
+window.setInterval(draw, 30);
