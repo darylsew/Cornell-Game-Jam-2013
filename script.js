7c444db70c6f4c451c4dae9e9cac11d7a565e398
Zachary Zee <Oneman2feet@gmail.com>
2013-09-14 02:38:34 -0400

    scrolling with dead zone and variable velocity

--- script.js
+++ script.js
@@ -2,14 +2,38 @@ var canvas = document.getElementsByTagName('canvas')[0];
 var context = canvas.getContext('2d');
 
 //enviornment vars
-var mousePos = {x:150,y:300};
+var mousePos = {x:0,y:0};
+var backpos = 0;
 
 function draw()
 {
+    var newX = backpos + getDbackpos();
+    backpos = ((newX<0) ? 0: ((newX>1600) ? 1600 : newX));
     $("canvas").drawImage({
         source: "resources/testbackground.jpg",
-        x: mousePos.x, y: 300
+        x: backpos, y: 300
     });
+}
+
+function getDbackpos()
+{
+    var mouseX = mousePos.x;
+    mouseX = (mouseX<0) ? 0 : ((mouseX>800) ? 800 : mouseX);
+    if (mouseX>=300 && mouseX<=500) return 0;
+
+    var vel;
+    if (mouseX<300)
+    {
+        vel = mouseX - 300;
+    }
+
+    if (mouseX>500)
+    {
+        vel = mouseX - 500;
+    }
+
+    return vel/10;
+
 }
 
 function getMousePos(canvas, evt)
@@ -24,6 +48,7 @@ function getMousePos(canvas, evt)
 canvas.addEventListener('mousemove', function(evt)
 {
     mousePos = getMousePos(canvas, evt);
+    console.log(mousePos.x);
 }, false);
 
 window.setInterval(draw, 30);
